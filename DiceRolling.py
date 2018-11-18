be3206e6471cf0cb488785d25d28013bd5ec588a
kindness86 <himantika.sharma@gmail.com>
2018-11-18 14:38:57 +0200

    Initial commit

---
+++ DiceRolling.py
@@ -0,0 +1,33 @@
+from random import randint
+from time import sleep
+
+def user_guess():
+    guess = int(raw_input("Enter your guess: ")) #if you do not enter int here then prog will give wrong output
+    return guess #telling to store the value of guess
+
+def dice_roll(sides_no):
+    first_roll = randint(1, sides_no)
+    second_roll = randint(1, sides_no)
+    max_no = sides_no * 2
+    total_no = first_roll + second_roll
+    print "The maximum value you could select is; %d" % (max_no)
+    guess = user_guess() #telling to go to user_guess function
+    if guess > max_no:
+        print "You cannot exceed beyond the specified value"
+    else:
+        print "Rolling....."
+        sleep(2)
+        print "The first roll is: %d" % (first_roll)
+        sleep(2)
+        print "The second roll is: %d" % (second_roll)
+        sleep(2)
+        print "The sum of both rolls is: %d" % (total_no)
+        print "Result..."
+        sleep(3)
+
+        if guess >= total_no:
+            print "Congrats! You win"
+        else:
+            print "Sorry! You Loose."
+
+dice_roll(6)
